de73e659d01b265b711b6d340a46f2b86e872694
Nathan <nathan.nguyen@invesco.com>
2014-04-15 12:32:48 -0500

    Table Controller added

---
+++ UITableViewController Challenge Solution/NNTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NNTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Nathan Nguyen on 4/15/14.
+//  Copyright (c) 2014 nigmia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NNTableViewController : UITableViewController
+
+@end
